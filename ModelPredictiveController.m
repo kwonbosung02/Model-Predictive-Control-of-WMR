686681d3cf373c7605a9427de26c46caa49b3c47
kwonbosung02 <rnjsqhtjd0526@naver.com>
2021-08-11 19:10:54 +0900

    commit

---
+++ ModelPredictiveController.m
@@ -0,0 +1,23 @@
+function ModelPredictiveController(states)
+
+    [idx,params] = get_params();
+    
+    
+    
+    [A,B] = get_DynamicMatrix(idx,params);
+
+   % [Acond, Bcond] = getCondensedMatrix(idx,params);
+    
+   % [ H, J ] = getCostFunction(idx,params);
+    
+   % [ Aineq, Bineq] = getInequalityConstraint(idx,params);
+    
+    
+%t = 0:0.01:10
+ %   x = 2 * sin
+  %  y = 2 - 2*cos
+
+
+   % dx 
+    
+end
---
+++ get_DynamicMatrix.m
@@ -0,0 +1,6 @@
+function [A, B] = get_DynamicMatrix(idx,params)
+    A = eye(3);
+    
+    B = eye(3);
+
+end
