5cc3f5a6b90bdf89006380a80252fe00a6ef7ac4
kwonbosung02 <rnjsqhtjd0526@naver.com>
2021-08-18 18:28:26 +0900

    commit

--- Main.m
+++ Main.m
@@ -15,8 +15,11 @@ hold on
 for simNum  = 1 : iterNum
     
      states = ModelPredictiveController(simNum,idx,params,states,data);
+     %ModelPredictiveController(simNum,idx,params,states,data);
+     
      plot(states(1),states(2),'-o')
      drawnow;
+     
 end
 
 
--- get_Data.m
+++ get_Data.m
@@ -7,15 +7,11 @@ t2 = 2*pi-0.025 : -0.01 : 0;
 X = horzcat(2*sin(t),-2*sin(t2));
 Y = horzcat(2*cos(t+pi)+2,-2*cos(t2+pi)-2);
 theta = horzcat(t,t2);
-%theta = t
-%X = 2*sin(t)
-%Y = 2*cos(t)
-%dX = 2*cos(t)
-%dY = -2*sin(t)
-%dX dY data
-dX = horzcat(2*cos(t),-2*cos(t2));
-dY = horzcat(-2*sin(t+pi),2*sin(t2+pi));
 
+%dX = horzcat(2*cos(t),-2*cos(t2));
+%dY = horzcat(-2*sin(t+pi),2*sin(t2+pi));
+dX = gradient(X);
+dY = gradient(Y);
 data.x = X;
 data.y = Y;
 data.theta= theta;
@@ -24,6 +20,7 @@ data.states = [X; Y; theta];
 data.dx = dX;
 data.dy = dY;
 
+data.w = gradient(theta);
 
 end
 
