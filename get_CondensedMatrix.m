a681ab5cdb5985cfe494080d62f46f19184ba729
kwonbosung02 <rnjsqhtjd0526@naver.com>
2021-08-18 16:47:21 +0900

    commit

--- get_CondensedMatrix.m
+++ get_CondensedMatrix.m
@@ -28,11 +28,9 @@ end
 Ax = 1;
 for j = 1 : idx.n_hor
     for i = 1 : idx.n_hor
-       if(i + j > idx.n_hor +1)
-           continue
-       end
        if(i>j)
-            Ax = Ax * A{j+i-1} ;
+           
+            Ax = Ax * A{i} ;
             Bcond{i,j} = Ax * Bcond{i,j} ;
        end
     end
